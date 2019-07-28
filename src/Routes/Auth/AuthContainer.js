8382a67df3eb7df2132d333b7a5fcf80c6b9dab2
jsh901220 <joe1220@daum.net>
2019-07-28 18:19:31 +0900

    Toastify and createAccount Mutation

--- src/Routes/Auth/AuthContainer.js
+++ src/Routes/Auth/AuthContainer.js
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import AuthPresenter from "./AuthPresenter"
 import useInput from "../../Components/Hooks/useInput"
 import { useMutation } from "react-apollo-hooks";
-import { LOG_IN } from "./AuthQueries";
+import { LOG_IN, CREATE_ACCOUNT } from "./AuthQueries";
+import { toast } from "react-toastify";
 
 export default () => {
   const [action, setAction] = useState("logIn");
@@ -11,14 +12,40 @@ export default () => {
   const lastName = useInput("");
   const email = useInput("");
   const [requestSecret] = useMutation(LOG_IN, {
+    update: (_, { data }) => {
+      const { requestSecret } = data
+      if(!requestSecret) {
+        toast.error("You dont have an account yet, create one")
+        setTimeout(() => setAction("signUp"), 2000);
+      }
+    },
     variables: { email: email.value }
   });
 
-  const onLogin = e => {
+  const [createAccount] = useMutation(CREATE_ACCOUNT, {
+    variables: {       
+      email: email.value,
+      username: username.value,
+      firstName: firstName.value,
+      lastName: lastName.value}
+  })
+
+  const onSubmit = e => {
     e.preventDefault();
-    if (email !== "") {
-      requestSecret();
+    if(action === "logIn") {
+      if (email !== "") {
+        requestSecret();
+      } else {
+        toast.error("Email is Required")
+      }
+    } else if(action === "signUp") {
+      if(email.value !== "" && username.value !== "" && firstName.value !== "" && lastName.value !== "") {
+        createAccount()
+      } else {
+        toast.error("All fields are Requried")
+      }
     }
+
   };
 
   return (
@@ -29,7 +56,7 @@ export default () => {
       firstName={firstName}
       lastName={lastName}
       email={email}
-      onLogin={onLogin}
+      onSubmit={onSubmit}
     />
   );
 };
